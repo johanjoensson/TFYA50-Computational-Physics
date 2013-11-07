210327ae564a72d62ee97dfa4b32910cb58cd712
Johan Jönsson <johjo939@student.liu.se>
2013-11-07 12:47:25 +0100

    small changes, nothing major

--- Code/Project_v0.01/Project_v0.01/atom.h
+++ Code/Project_v0.01/Project_v0.01/atom.h
@@ -12,8 +12,8 @@ public:
 	atom(vector_3d position, vector_3d velocity, vector_3d acceleration);
 
 	void set_next_acc(vector_3d new_acc);
-	void atom::incr_next_acc(vector_3d new_acc);
-	void atom::decr_next_acc(vector_3d new_acc);
+	void incr_next_acc(vector_3d new_acc);
+	void decr_next_acc(vector_3d new_acc);
 
 	void incr_displacement(float r);
 	void zero_displacement();
