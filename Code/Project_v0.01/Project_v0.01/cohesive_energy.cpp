15bc0b3ab91a17793fd140d79884f6028be09ab4
simonwallin89 <simonwallin89@gmail.com>
2013-11-12 10:59:11 +0100

    The cpp file is working but i canot test the h file. It might not be needed though. /SW

---
+++ Code/Project_v0.01/Project_v0.01/cohesive_energy.cpp
@@ -0,0 +1,13 @@
+#include "cohesive_energy.h"
+#include <iostream>
+
+/*
+*N is here the total number of atoms
+*totEnergy is the total energy (kinetic + potential)
+*In the original equation the energy of the fre atom is included
+*but that shoudn't be nessesary.
+*/
+float cohEnergy (int N, float totEnergy){
+	float cE = N/totEnergy;
+	return(cE);
+}
