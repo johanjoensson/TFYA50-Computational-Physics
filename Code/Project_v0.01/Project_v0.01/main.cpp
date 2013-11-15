7f354280b01e08dc26f31f57da22db6fd256d9b9
simonwallin89 <simonwallin89@gmail.com>
2013-11-15 09:35:40 +0100

    I took away some old code that isn't used in main. /SW

--- Code/Project_v0.01/Project_v0.01/main.cpp
+++ Code/Project_v0.01/Project_v0.01/main.cpp
@@ -67,23 +67,14 @@ int main()
 
 //			std::cout << test.bulk[i].data->pos << "\t";
 			test.verlet_integrator.verlet_integration_position(test.bulk[i]);
-			//calculate the msd
+
 			r_msd += test.msd(test.atoms[i], test.N);
-			//calculate the kinetic energy
-			//REMOVE? - E_kin += (test.atoms[i].mass*test.atoms[i].vel*test.atoms[i].vel)/2;
-
-
-			//Print the MSD to track that it's working
-//			std::cout << "------> MSD: " << r_msd << std::endl;
 
 			if(test.bulk[i].data->get_displacement() > max_disp){
 				max_disp = test.bulk[i].data->get_displacement();
 			}
 		}
-//		std::cout << "------> Debye Temp: " << test.debye_temp(r_msd, 50, 1) << std::endl;
-//		std::cout << "------> Energy tot: " <<  test.verlet_integrator.e_pot+0.5*test.get_kinetic_energy() << std::endl;
-//		std::cout << "------> CohEnergy: " <<  test.cohEnergy(test.N, (test.verlet_integrator.e_pot+0.5*test.get_kinetic_energy())) << std::endl;
-
+		
 //		std::cout << std::endl;
 		for(unsigned int i = 0; i < test.N; i++){
 //			std::cout << "Calculating force on particle: " << i << std::endl;
